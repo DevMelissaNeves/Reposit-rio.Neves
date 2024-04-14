60c148b9aa40096e50739dcb0065f58b6059d848
Mel🍯 <166963422+DevMelissaNeves@users.noreply.github.com>
2024-04-14 19:43:21 -0300

    Update and rename Q05 - Utilizando dicionários para inserir o nome de três funcionários to Q05 - Utilizando dicionários para inserir o nome de três funcionários.py

--- DicionáriosEmPython/Q05 - Utilizando dicionários para inserir o nome de três funcionários.py
+++ DicionáriosEmPython/Q05 - Utilizando dicionários para inserir o nome de três funcionários.py
@@ -1,6 +1,4 @@
-#5. Faça um programa, utilizando Dicionários, que:
-1° Passo: Peça para o usuário inserir o nome de três funcionários e os mostre na tela.
-2° Passo: Peça para o usuário demitir um funcionário e mostre na tela os funcionários restantes.
+#5. Faça um programa, utilizando Dicionários, que: 1° Passo: Peça para o usuário inserir o nome de três funcionários e os mostre na tela. 2° Passo: Peça para o usuário demitir um funcionário e mostre na tela os funcionários restantes.
 
 funcionarios = {}
 
