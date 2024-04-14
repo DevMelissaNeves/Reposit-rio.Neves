90e4be013c1aecf7de42748afc7f0f8104bbbfd9
Mel🍯 <166963422+DevMelissaNeves@users.noreply.github.com>
2024-04-14 19:57:13 -0300

    Update and rename Q04 - Lê duas notas parciais de um aluno to Q04 - Lê duas notas parciais de um aluno.py

--- EstruturasDeComparação/Q04 - Lê duas notas parciais de um aluno.py
+++ EstruturasDeComparação/Q04 - Lê duas notas parciais de um aluno.py
@@ -1,7 +1,7 @@
 #4 - Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno apresentar:
-A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; 
-A mensagem "Reprovado", se a média for menor do que sete;
-A mensagem "Aprovado com Distinção", se a média for igual a dez.
+#A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; 
+#A mensagem "Reprovado", se a média for menor do que sete;
+#A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
