371f0f07601c11c42647074e9ccfd32da728f007
Mel🍯 <166963422+DevMelissaNeves@users.noreply.github.com>
2024-04-14 20:17:50 -0300

    Update Q07 - Recebe dois números inteiros e gera o número que estão no intervalo entre eles.py

--- EstruturaDeRepetição/Q07 - Recebe dois números inteiros e gera o número que estão no intervalo entre eles.py
+++ EstruturaDeRepetição/Q07 - Recebe dois números inteiros e gera o número que estão no intervalo entre eles.py
@@ -1,5 +1,4 @@
-#7 - Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
-Altere o programa anterior para mostrar no final a soma dos números.
+#7 - Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles. Altere o programa anterior para mostrar no final a soma dos números.
 
 def gerar_e_calcular_soma(num1, num2):
     inicio_intervalo = min(num1, num2)
