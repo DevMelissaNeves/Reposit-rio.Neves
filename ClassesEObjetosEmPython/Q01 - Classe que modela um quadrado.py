f814739c998280e840b84afdc3d66aa31bf45678
Mel🍯 <166963422+DevMelissaNeves@users.noreply.github.com>
2024-04-14 19:40:09 -0300

    Update Q01 - Classe que modela um quadrado.py

--- ClassesEObjetosEmPython/Q01 - Classe que modela um quadrado.py
+++ ClassesEObjetosEmPython/Q01 - Classe que modela um quadrado.py
@@ -1,6 +1,4 @@
-#1 - Crie uma classe que modele um quadrado:
-Atributos: Tamanho do lado
-Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+#1 - Crie uma classe que modele um quadrado: Atributos: Tamanho do lado Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
 class Quadrado:
     def __init__(self, lado):
